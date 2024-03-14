5d3a85e33e8f20e9d5a42b76c654168bcc39dca7
Esveraldo <esveraldo@hotmail.com>
2024-03-14 16:14:24 -0300

    Estrutura inicial do projeto

---
+++ TasksApp.Domain.Core/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksApp.Domain.Core
+{
+    public interface IEntity<TKey>
+    {
+        public TKey Id { get; set; }
+
+        ValidationResult Validate { get; }
+    }
+}
